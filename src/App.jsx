aa81f26941bbb02834385abe5c704797ad160c09
Diego Presiga <dialtordev@gmail.com>
2022-12-28 12:26:12 -0500

    fetching data coins

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get(url)
+    .then((res) => {
+      setCoins(res.data)
+    })
+  }, [])
+  
+  
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+         {
+          coins.map(coin => (
+            <h3 key={coin.id}>{coin.name}</h3>
+          ))
+         }
     </div>
   )
 }
