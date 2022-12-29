9f2ad4eb0efd56600c317c8f68261949cbb2c420
Facundo Carrion <facundojcarrion@gmail.com>
2022-12-28 22:25:06 -0300

    feature(app): better styling, backgrounds and color

--- src/components/BasicTable.jsx
+++ src/components/BasicTable.jsx
@@ -7,42 +7,33 @@ import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
 
+const styles = {
+  head: {
+    fontSize: '1rem',
+    fontWeight: 600,
+    backgroundColor: '#1f1717',
+    color: '#efefef'
+  }
+}
+
 const BasicTable = ({ coins }) => {
   return (
-    <TableContainer component={Paper}>
-      <Table stickyHeader sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableContainer component={Paper} style={{ borderRadius: '0' }}>
+      <Table stickyHeader sx={{ minWidth: 650 }} style={{ background: '#ededed' }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell style={{ fontSize: '1rem', fontWeight: 600 }}>
-              #
-            </TableCell>
-            <TableCell style={{ fontSize: '1rem', fontWeight: 600 }}>
-              Icon
-            </TableCell>
-            <TableCell style={{ fontSize: '1rem', fontWeight: 600 }}>
-              Name
-            </TableCell>
-            <TableCell style={{ fontSize: '1rem', fontWeight: 600 }}>
-              Current Price
-            </TableCell>
-            <TableCell style={{ fontSize: '1rem', fontWeight: 600 }}>
-              Last Reload
-            </TableCell>
-            <TableCell style={{ fontSize: '1rem', fontWeight: 600 }}>
-              Market Cap
-            </TableCell>
-            <TableCell style={{ fontSize: '1rem', fontWeight: 600 }}>
-              Circulating Supply
-            </TableCell>
+            <TableCell style={styles.head}>#</TableCell>
+            <TableCell style={styles.head}>Icon</TableCell>
+            <TableCell style={styles.head}>Name</TableCell>
+            <TableCell style={styles.head}>Current Price</TableCell>
+            <TableCell style={styles.head}>Last Reload</TableCell>
+            <TableCell style={styles.head}>Market Cap</TableCell>
+            <TableCell style={styles.head}>Circulating Supply</TableCell>
           </TableRow>
         </TableHead>
-
         <TableBody>
           {coins.map((coin, index) => (
-            <TableRow
-              key={coin.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
+            <TableRow key={coin.name} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
               <TableCell component="th" scope="row" width={'0.1%'}>
                 {index + 1}
               </TableCell>
@@ -62,13 +53,13 @@ const BasicTable = ({ coins }) => {
                 </span>
               </TableCell>
               <TableCell align="left" width={'12%'}>
-                {'$' + coin.current_price}
+                {'$ ' + coin.current_price}
               </TableCell>
               <TableCell align="left" width={'14%'}>
                 {coin.last_updated.slice(0, -5).replace('T', ' ')}
               </TableCell>
               <TableCell align="left" width={'12%'}>
-                {coin.market_cap}
+                {'$ ' + coin.market_cap}
               </TableCell>
               <TableCell align="left" width={'12%'}>
                 {coin.circulating_supply}
